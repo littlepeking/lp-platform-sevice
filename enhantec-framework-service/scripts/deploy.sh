83adfe78ac60f8d51ad42ad56952236c9d17a46d
John Wang <john.wang_ca@hotmail.com>
2022-10-17 13:17:45 -0400

    minor change

--- enhantec-framework-service/scripts/deploy.sh
+++ enhantec-framework-service/scripts/deploy.sh
@@ -1,3 +1,4 @@
 CLOUDSMITH_REPOSITORY=eh-xyp/enhantec-platform-service
 
-cloudsmith push maven $CLOUDSMITH_REPOSITORY --pom-file ../pom.xml ../target/enhantec-framework-service-$1.jar
+
+cloudsmith push maven $CLOUDSMITH_REPOSITORY --pom-file ../pom.xml --pom-file ../../pom.xml ../target/enhantec-framework-service-$1.jar
