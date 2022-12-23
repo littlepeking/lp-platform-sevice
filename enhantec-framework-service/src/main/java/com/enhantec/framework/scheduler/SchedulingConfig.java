32bdbff7711c5383a5e3f45ab8e6a93a91567e27
John Wang <john.wang_ca@hotmail.com>
2022-12-23 11:17:35 -0500

    init job

---
+++ enhantec-framework-service/src/main/java/com/enhantec/framework/scheduler/SchedulingConfig.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) 2022, Enhantec
+ * All rights reserved.
+ * <p>
+ * Author: John Wang
+ * Email: john.wang_ca@hotmail.com
+ */
+
+package com.enhantec.framework.scheduler;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class SchedulingConfig {
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        // 定时任务执行线程池核心线程数
+        taskScheduler.setPoolSize(30);
+        taskScheduler.setRemoveOnCancelPolicy(true);
+        taskScheduler.setThreadNamePrefix("TaskSchedulerThreadPool-");
+        return taskScheduler;
+    }
+}
