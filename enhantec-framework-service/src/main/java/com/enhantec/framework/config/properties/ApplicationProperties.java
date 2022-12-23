9b9b1e9ee712d154c38fa52a971485c898ef65fe
John Wang <john.wang_ca@hotmail.com>
2022-12-23 16:20:34 -0500

    move bean config

--- enhantec-framework-service/src/main/java/com/enhantec/framework/config/properties/ApplicationProperties.java
+++ enhantec-framework-service/src/main/java/com/enhantec/framework/config/properties/ApplicationProperties.java
@@ -27,6 +27,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Collections;
 import java.util.List;
--- enhantec-framework-service/src/main/java/com/enhantec/framework/security/core/SecurityBeansConfig.java
+++ enhantec-framework-service/src/main/java/com/enhantec/framework/security/core/SecurityBeansConfig.java
@@ -1,40 +1,30 @@
-/*******************************************************************************
- *                                     NOTICE
- *
- *             THIS SOFTWARE IS THE PROPERTY OF AND CONTAINS
- *             CONFIDENTIAL INFORMATION OF Shanghai Enhantec Information
- *             Technology Co., Ltd. AND SHALL NOT BE DISCLOSED WITHOUT PRIOR
- *             WRITTEN PERMISSION. LICENSED CUSTOMERS MAY COPY AND
- *             ADAPT THIS SOFTWARE FOR THEIR OWN USE IN ACCORDANCE WITH
- *             THE TERMS OF THEIR SOFTWARE LICENSE AGREEMENT.
- *             ALL OTHER RIGHTS RESERVED.
- *
- *             (c) COPYRIGHT 2022 Enhantec. ALL RIGHTS RESERVED.
- *
- *******************************************************************************/
-
 /**
+ * Copyright (C) 2022, Enhantec
+ * All rights reserved.
+ * <p>
  * Author: John Wang
- * john.wang_ca@hotmail.com
+ * Email: john.wang_ca@hotmail.com
  */
 
+package com.enhantec.framework.security.core;
 
-
-package com.enhantec.framework.security.common;
-
+import com.enhantec.framework.config.properties.ApplicationProperties;
 import com.enhantec.framework.security.common.service.RoleHierarchyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 @RequiredArgsConstructor
 @Slf4j
-public class SecurityBeans {
+@EnableConfigurationProperties(ApplicationProperties.class)
+public class SecurityBeansConfig {
 
     private final RoleHierarchyService roleHierarchyService;
 
