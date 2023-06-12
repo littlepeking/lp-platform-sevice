11ddcce7f330d1e3d5a9feacedbbd7af3dd95135
John Wang <john.wang_ca@hotmail.com>
2023-06-12 10:01:59 -0400

    minor change

--- enhantec-framework-service/scripts/deploy.sh
+++ enhantec-framework-service/scripts/deploy.sh
@@ -1,6 +1,10 @@
-CLOUDSMITH_REPOSITORY=eh-xyp/enhantec-framework-service
+#CLOUDSMITH_REPOSITORY=eh-xyp/enhantec-framework-service
+#
+#cloudsmith push maven $CLOUDSMITH_REPOSITORY --pom-file ../../pom.xml ../../enhantec-framework-parent-service.jar
+#
+#
+#cloudsmith push maven $CLOUDSMITH_REPOSITORY --pom-file ../pom.xml ../target/enhantec-framework-service-$1.jar
 
-cloudsmith push maven $CLOUDSMITH_REPOSITORY --pom-file ../../pom.xml ../../enhantec-framework-parent-service.jar
 
 
-cloudsmith push maven $CLOUDSMITH_REPOSITORY --pom-file ../pom.xml ../target/enhantec-framework-service-$1.jar
+## Replaced cloudSmith Cli and deploy by run "mvn clean deploy" in parent folder.
